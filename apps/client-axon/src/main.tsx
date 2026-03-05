2c7e55d4b843f8ac7d5daddf1d88623d8ca495b7
Tom Britton <tom.jf.britton@gmail.com>
2026-03-05 17:30:45 +0000

    fixed main

--- apps/client-axon/src/main.tsx
+++ apps/client-axon/src/main.tsx
@@ -1,17 +1,9 @@
 import { App } from "@app/App";
-import { persistor, store } from "@app/store";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-          <App/>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
