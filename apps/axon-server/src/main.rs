6e738a5cd383971f6ae921de273cc4169c493f1c
Tom Britton <tom.jf.britton@gmail.com>
2026-03-09 21:47:30 +0000

    trigger build

--- apps/axon-server/src/main.rs
+++ apps/axon-server/src/main.rs
@@ -3,12 +3,11 @@ mod infrastructure;
 mod state;
 
 use crate::infrastructure::prelude::*;
-// use tracing::info;
 
 #[tokio::main]
 async fn main() -> AxonResult<()> {
     tracing_subscriber::fmt()
-  .with_env_filter("axon=debug,axum_keycloak_auth=debug") 
+    .with_env_filter("axon=debug,axum_keycloak_auth=debug") 
     .init();
     Bootstrapper::<Unconfigured>::new()
         .await?
