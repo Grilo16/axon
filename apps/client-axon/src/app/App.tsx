71413dbd25305a35bef7307820387819fe255fb7
Tom Britton <tom.jf.britton@gmail.com>
2026-03-11 16:45:21 +0000

    added og image, and mobile bouncer for now

--- apps/client-axon/src/app/App.tsx
+++ apps/client-axon/src/app/App.tsx
@@ -1,10 +1,13 @@
+import { MobileBouncer } from "@shared/ui/components/mobile-bouncer";
 import { AppProviders } from "./providers/app-providers";
 import { AppRoutes } from "./routes/app-routes";
 
 export const App = () => {
   return (
     <AppProviders>
-      <AppRoutes />
+      <MobileBouncer>
+        <AppRoutes />
+      </MobileBouncer>
     </AppProviders>
   );
 };
